e7dfab75ca9dffdf70edd8b51651b392469f80c9
yuski <yuski.tw@gmail.com>
2017-11-07 23:38:34 +0800

    Update 2017-11-07T15:38:30.065Z

---
+++ js/all.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var newFn = function newFn() {
+    console.log('hello');
+};
+newFn();
+'use strict';
+
+var newFn2 = function newFn2() {
+    console.log('b');
+};
+newFn2();
+//# sourceMappingURL=all.js.map
